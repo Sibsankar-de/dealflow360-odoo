2f9cba0eb4e0a95f97790503d6ecacda201459fa
Sibsankar-de <sibsankarde910@gmail.com>
2026-09-05 16:27:49 +0530

    fix: quotation status (expired) removed

---
+++ server/prisma/migrations/20260905105715/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - The values [EXPIRED] on the enum `QuotationStatus` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "QuotationStatus_new" AS ENUM ('DRAFT', 'SENT', 'ACCEPTED', 'REJECTED', 'CANCELLED', 'UNDER_NEGOTIATION');
+ALTER TABLE "public"."quotations" ALTER COLUMN "status" DROP DEFAULT;
+ALTER TABLE "quotations" ALTER COLUMN "status" TYPE "QuotationStatus_new" USING ("status"::text::"QuotationStatus_new");
+ALTER TYPE "QuotationStatus" RENAME TO "QuotationStatus_old";
+ALTER TYPE "QuotationStatus_new" RENAME TO "QuotationStatus";
+DROP TYPE "public"."QuotationStatus_old";
+ALTER TABLE "quotations" ALTER COLUMN "status" SET DEFAULT 'DRAFT';
+COMMIT;
